bffaa394ec9731ae5f119136945582f6568f5a91
Wang Yu Xiang <visolleon@gmail.com>
2012-11-01 14:56:08 +0800

    e

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,14 @@
-console.log('This would be the main JS file.');
+(function (g) {
+	g.onload = function () {
+		var w = document.getElementById('words');
+			p = document.getElementById('poster');
+		setTimeout(function () {
+			$(w).addClass('animated bounceOut');
+			setTimeout(function () {
+				$(w).remove();
+				$(p).show();
+				$(p).addClass('animated bounceInUp');
+			}, 1000)
+		}, 3000);
+	}
+})(window);
