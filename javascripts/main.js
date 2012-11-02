7599d5d186ee71bde4b03094642d1a62f5e575fc
Wang Yu Xiang <visolleon@gmail.com>
2012-11-02 09:24:03 +0800

    s

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,14 +1,22 @@
 (function (g) {
 	g.onload = function () {
+		var ie = !!window.ActiveXObject;
 		var w = document.getElementById('words');
 			p = document.getElementById('poster');
-		setTimeout(function () {
-			$(w).addClass('animated bounceOut');
+
+		if(ie) {
+			$(w).hide();
+			$(p).show();
+		}
+		else {
 			setTimeout(function () {
-				$(w).remove();
-				$(p).show();
-				$(p).addClass('animated bounceInUp');
-			}, 1000)
-		}, 3000);
+				$(w).addClass('animated bounceOut');
+				setTimeout(function () {
+					$(w).remove();
+					$(p).show();
+					$(p).addClass('animated bounceInUp');
+				}, 1000)
+			}, 1500);
+		}
 	}
 })(window);
